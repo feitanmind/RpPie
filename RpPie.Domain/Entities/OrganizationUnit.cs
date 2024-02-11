da50020a27a7786723972ac187c54f5d04f2c069
Adam Burski <burskiadamwork@gmail.com>
2024-02-11 08:30:25 +0100

    Utworzenie modelu OrganizationUnit oraz OrganizationUnitContactDetails

---
+++ RpPie.Domain/Entities/OrganizationUnit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpPie.Domain.Entities
+{
+    public class OrganizationUnit
+    {
+        public required int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public OrganizationUnitContactDetails ContactDetails { get; set; } = default!;
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ", "-");
+    }
+}
---
+++ RpPie.Domain/Entities/OrganizationUnitContactDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpPie.Domain.Entities
+{
+    public class OrganizationUnitContactDetails
+    {
+        public string? PhoneNumber { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+    }
+}
