50d58b0da5e8e1334f7261592988ab4d1a5eb6dd
Adam Burski <burskiadamwork@gmail.com>
2024-02-11 09:49:46 +0100

    Create connetion to db

---
+++ RpPie.Infrastucture/Persistance/RpPieDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RpPie.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpPie.Infrastucture.Persistance
+{
+    public class RpPieDbContext : DbContext
+    {
+        public DbSet<OrganizationUnit> OrganizationUnits { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=sql.bsite.net\\MSSQL2016;Database=rppie_db;User id=rppie_db;Password=6.QkD,Q@@dST]KN;Encrypt=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrganizationUnit>()
+                .OwnsOne(c => c.ContactDetails);
+        }
+    }
+}
--- RpPie/Program.cs
+++ RpPie/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RpPie.Infrastucture.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<RpPieDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("RpPie")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
